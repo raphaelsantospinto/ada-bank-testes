df4e6617765c967af1f81d9961100fa573a07532
Raphael dos Santos Pinto <raphaelsantospinto@gmail.com>
2024-06-18 15:42:57 -0300

    Commit Inicial

---
+++ src/test/java/br/ada/caixa/service/cliente/ClienteServiceTest.java
@@ -0,0 +1,38 @@
+package br.ada.caixa.service.cliente;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClienteServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void registrarPF() {
+    }
+
+    @Test
+    void registrarPJ() {
+    }
+
+    @Test
+    void registrar() {
+    }
+
+    @Test
+    void listarTodos() {
+    }
+
+    @Test
+    void testListarTodos() {
+    }
+}
